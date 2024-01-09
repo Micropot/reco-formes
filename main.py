bd726f2bc99b7f2e514a3e2185d8a1af779bc7ac
Micropot <arthur@lamard.org>
2024-01-09 14:23:59 +0100

    src dir

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
@@ -8,4 +7,3 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
