b33abf2ae373fc6f505aac3d48b02755eebc8319
Micropot <arthur@lamard.org>
2024-01-09 14:11:58 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+
+
+# utilsier roboflow (2000 images) for training
+# utiliser yolov8 pour de la segmentation d'images
+# faire un masque -> copier et coller ce masque sur une autre image
+# ne pas utisier la segmentation deja faite par yolov8, il faut entrainer a la main
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
